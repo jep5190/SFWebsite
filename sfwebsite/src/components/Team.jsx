b04e3a1550b29dcd0ef83af2511e9ee55479efff
Jeremy P <geniusjp08@gmail.com>
2022-05-02 14:30:47 -0400

    Initial checkin of new website with npm hopefully working

---
+++ sfwebsite/src/components/Team.jsx
@@ -0,0 +1,30 @@
+export const Team = (props) => {
+  return (
+    <div id='team' className='text-center'>
+      <div className='container'>
+        <div className='col-md-8 col-md-offset-2 section-title'>
+          <h2>Meet the Team</h2>
+          <p>
+            Small family owned and run business.
+          </p>
+        </div>
+        <div id='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className='col-md-6 col-sm-6 team'>
+                  <div className='thumbnail'>
+                    {' '}
+                    <img src={d.img} alt='...' className='team-img' />
+                    <div className='caption'>
+                      <h4>{d.name}</h4>
+                      <p>{d.job}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  )
+}
