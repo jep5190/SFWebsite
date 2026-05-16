63710b7796184cfa5b99ac662b0f9fae7ac83537
Jeremy P <geniusjp08@gmail.com>
2026-05-15 21:05:02 -0400

    Fix overflow issue and enable yarn deploy

--- sfwebsite/src/components/services.jsx
+++ sfwebsite/src/components/services.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export const Services = (props) => {
   return (
     <div id='services' className='text-center'>
@@ -12,14 +14,19 @@ export const Services = (props) => {
         <div className='row'>
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className='col-md-4'>
-                  {' '}
-                  <i className={d.icon}></i>
-                  <div className='service-desc'>
-                    <h3>{d.name}</h3>
-                    <p>{d.text}</p>
+                <React.Fragment key={`${d.name}-${i}`}>
+                  <div className='col-md-4'>
+                    {' '}
+                    <i className={d.icon}></i>
+                    <div className='service-desc'>
+                      <h3>{d.name}</h3>
+                      <p>{d.text}</p>
+                    </div>
                   </div>
-                </div>
+                  {i === 2 && (
+                    <div className='clearfix visible-md-block visible-lg-block'></div>
+                  )}
+                </React.Fragment>
               ))
             : 'loading'}
         </div>
